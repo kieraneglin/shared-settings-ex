3de5d4e701e25badcbb7cc744a934f5f22a6c088
Kieran Eglin <kieran.eglin@gmail.com>
2020-08-26 15:28:39 -0700

    More Redis CI testings

--- lib/shared_settings/persistence/redis.ex
+++ lib/shared_settings/persistence/redis.ex
@@ -10,7 +10,7 @@ if Code.ensure_loaded?(Redix) do
     @default_redis_config [
       name: @conn,
       sync_connect: false,
-      host: "redis",
+      host: "localhost",
       port: 6379,
       database: 0
     ]
