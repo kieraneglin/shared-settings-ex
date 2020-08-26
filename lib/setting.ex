6ece1dc69695868ed944c24b6073bbf3df27833e
Kieran Eglin <kieran.eglin@gmail.com>
2020-08-26 10:39:51 -0700

    added methods for building a setting for various types

--- lib/setting.ex
+++ lib/setting.ex
@@ -1,6 +1,60 @@
 defmodule SharedSettings.Setting do
+  @moduledoc """
+  Provides a struct/type for the settings that go in and out of storage.
+
+  The methods here aren't intended to be used on their own.
+  SharedSettings should be the module you interface with
+  """
+
+  alias __MODULE__
+
   @enforce_keys [:name, :type, :value]
   defstruct [:name, :type, :value]
 
   @type t :: %SharedSettings.Setting{name: String.t(), type: String.t(), value: String.t()}
+
+  # I'm using conditionals within the block instead of guards since there may be
+  # other reasons the guards don't match that don't strictly imply that the
+  # wrong type/value combo was passed.
+  def build_setting(name, :string, value) do
+    if is_binary(value) do
+      {:ok, %Setting{name: name, type: "string", value: value}}
+    else
+      {:error, :incompatible_type}
+    end
+  end
+
+  def build_setting(name, :number, value) do
+    if is_integer(value) || is_float(value) do
+      stringified_value = to_string(value)
+
+      {:ok, %Setting{name: name, type: "number", value: stringified_value}}
+    else
+      {:error, :incompatible_type}
+    end
+  end
+
+  def build_setting(name, :boolean, value) do
+    if is_boolean(value) do
+      stringified_value = if value, do: "1", else: "0"
+
+      {:ok, %Setting{name: name, type: "boolean", value: stringified_value}}
+    else
+      {:error, :incompatible_type}
+    end
+  end
+
+  def build_setting(name, :range, value) do
+    if is_range(value) do
+      first..last = value
+      stringified_value = "#{first},#{last}"
+
+      {:ok, %Setting{name: name, type: "range", value: stringified_value}}
+    else
+      {:error, :incompatible_type}
+    end
+  end
+
+  defp is_range(%Range{}), do: true
+  defp is_range(_), do: false
 end
--- lib/shared_settings.ex
+++ lib/shared_settings.ex
@@ -1,2 +1,20 @@
 defmodule SharedSettings do
+  @moduledoc """
+  SharedSettings is a library for fetching and updating settings at runtime.
+
+  The goal of this is to provide a simple, language-agnostic storage interface
+  as well as an accompanying Ruby gem (TODO) and UI (TODO).  This is not intended
+  to be a fully-fledged feature flagging library (see FunWithFlags if you need that).
+  Instead, this is geared toward updating settings represented by a string, integer, etc.,
+  for the purpose of easing runtime tweaking of knobs.
+  """
+
+  # alias SharedSettings.Setting
+
+  # TODO: swap this out for config once we have >1 storage adapter
+  # @store SharedSettings.Cache.EtsStore
+
+  # def create(name, type, value) when is_atom(name) and is_atom(type) do
+
+  # end
 end
